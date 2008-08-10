93e7651829536fcdbf7d48589307b4fff2ea6ab0
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2008-08-10 17:36:07 -0500

    dir restructures

---
+++ python/THWAP/logging/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+class Log:
+	def __init__(self, name=''):
+		logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(levelname)s %(message)s',filename='/tmp/myapp.log',filemode='w')
+
+	def error(self, msg=''):
+		if msg != '': logging.debug(msg.strip())
+	
+	def debug(self, msg=''):
+		if msg != '': logging.debug(msg.strip())
+
+	def info(self, msg=''):
+		if msg != '': logging.info(msg.strip())
+
+	def warning(self, msg=''):
+		if msg != '': logging.warning(msg.strip())
+
