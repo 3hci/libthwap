795f557ad33afbb10e3da8f7ff0bcc8b37cdbf62
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2008-08-13 13:26:24 -0500

    ruby implimentation of thSlurp class in config.rb

---
+++ ruby/config.rb
@@ -0,0 +1,27 @@
+class thSlurp
+	def initialize
+			@triggers = []
+	end
+
+	def regCallBack(pattern, callback)
+		if pattern == nil or callback == nil
+				puts 'dumbass'
+		else
+			@triggers.push({'pattern' => pattern, 'callback' => callback})
+		end
+	end
+
+	def run(file)
+		if file == nil
+			puts 'dumbass'
+		else
+			File.open(file).each { |line|
+				@triggers.each { |trigger|
+					if line =~ trigger.pattern
+						puts "#{line} - #{trigger.pattern.to_s}"
+					end
+				}
+			}
+		end
+	end
+end
