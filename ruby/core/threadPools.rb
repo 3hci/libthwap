e44c51cbccdcd50cd17a8251727dc78a3afb19da
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2008-08-20 14:00:16 -0500

    added 2 remaining thread/threadPool classes to ruby/core/threadPools.rb

--- ruby/core/threadPools.rb
+++ ruby/core/threadPools.rb
@@ -8,3 +8,36 @@ class ThSpawnThread
 		t.join
 	end
 end
+
+class ThThreadObj
+	def initialize
+		@function = nil
+		@args = ()
+	end
+
+	def regWorkLoad(func, args)
+		if @function == nil
+			@function = func
+		end
+		if @args == ()
+			@args.push(args)
+		end
+	end
+
+	def run
+		ThSpawnThread.new(@function, @args)
+	end
+end
+
+# Class: ThThreadPool
+# Author: Mike "Fuzzy" Partin
+# Notes: Not done yet, but will be a pool of generic threads
+class ThThreadPool
+	def initialize(threads)
+		@threadPool = ()
+	  if threads.is_a?(Integer)
+			threads.downto(1) { |iter|
+				@threadPool.push(ThThreadObj.new)
+			}
+		end
+	end
