b0f9c0f6a7ca65867bc5488ac0dfb4c156dd6fb3
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2008-08-22 16:37:39 -0500

    error checking on current unit tests in tests/thwap-tests.py

--- tests/thwap-tests.py
+++ tests/thwap-tests.py
@@ -5,8 +5,10 @@ sys.path.insert(0, '%s/../python/' % os.getenv('PWD'))
 # python/THWAP/core/config.py unit tests
 from THWAP.core import config
 # thSlurp tests
+thSlurpFlag = 0
 def mprint(msg):
-		print msg
+		global thSlurpFlag
+		thSlurpFlag = 1
 
 thSlurp = config.thSlurp()
 fp = open('/tmp/thwap-py-unit-test-slurp', 'w+')
@@ -14,8 +16,11 @@ fp.write('this is a test\nbut this is not\n')
 fp.close()
 fp = open('/tmp/thwap-py-unit-test-slurp','r')
 thSlurp.registerTrigger('^this.*', mprint)
-thSlurp.registerTrigger('^but.*', mprint)
 thSlurp.process(fp)
+if thSlurpFlag == 1:
+	print 'thSlurp interface works as expected.' 
+else:
+	print 'thSlurp interface is not working as expected.'
 os.unlink('/tmp/thwap-py-unit-test-slurp')
 
 # thConfig tests
@@ -25,9 +30,12 @@ fp.write('''testSection {
 }''')
 fp.close()
 thConfig = config.thConfig('/tmp/thwap-py-unit-test-config')
-print thConfig.conf
-print thConfig.lookup('testSection', 'testkey')
-thConfig.set('testSection', 'testkey', 'anotherValue')
-print thConfig.conf
-print thConfig.lookup('testSection', 'testkey')
+if thConfig.conf != {} and thConfig.lookup('testSection', 'testkey') == 'testvalue':
+	thConfig.set('testSection', 'testkey', 'anotherValue')
+	if thConfig.lookup('testSection', 'testkey') == 'anotherValue':
+		print 'thConfig interface works as expected.'
+	else:
+		print 'thConfig interface is not working as expected.'
+else:
+	print 'thConfig interface is not working as expected.'
 os.unlink('/tmp/thwap-py-unit-test-config')
